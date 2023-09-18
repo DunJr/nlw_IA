1c7bcc8061a9f1c652d6b2023ac024064f387515
Carlos Roberio <Carlosroberiojp@hotmail.com>
2023-09-18 15:46:56 -0300

    Revert "Update"
    This reverts commit 58b4719aac47122cfff8d9e98956b9e233791496.

--- web/server.js
+++ web/server.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 
 export const server = axios.create({
-  baseURL: "http://shortssummarizer-898038330acc.herokuapp.com/",
-})
+  baseURL: "http://localhost:3333/",
+});
