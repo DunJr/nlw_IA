8438239d1f19cf19e5151ff005736e53219bc3af
Carlos Roberio <Carlosroberiojp@hotmail.com>
2023-09-18 14:56:07 -0300

    Update

--- server/index.js
+++ server/index.js
@@ -1,39 +1,41 @@
-import cors from "cors";
-import express from "express";
+import cors from "cors"
+import express from "express"
 
-import { convert } from "./convert.js";
-import { download } from "./download.js";
-import { transcribe } from "./transcribe.js";
-import { summarize } from "./summarize.js";
+import { convert } from "./convert.js"
+import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
 
-import "dotenv/config";
+import "dotenv/config"
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express()
+app.use(express.json())
+app.use(cors())
 
 app.get("/summary/:id", async (request, response) => {
   try {
-    await download(request.params.id);
-    const audioConverted = await convert();
+    await download(request.params.id)
+    const audioConverted = await convert()
 
-    const result = await transcribe(audioConverted);
+    const result = await transcribe(audioConverted)
 
-    return response.json({ result });
+    return response.json({ result })
   } catch (error) {
-    console.log(error);
-    return response.json({ error });
+    console.log(error)
+    return response.json({ error })
   }
-});
+})
 
 app.post("/summary", async (request, response) => {
   try {
-    const result = await summarize(request.body.text);
-    return response.json({ result });
+    const result = await summarize(request.body.text)
+    return response.json({ result })
   } catch (error) {
-    console.log(error);
-    return response.json({ error });
+    console.log(error)
+    return response.json({ error })
   }
-});
+})
 
-app.listen(3333, () => console.log("Server is running on port 3333"));
+app.listen(process.env.PORT, () =>
+  console.log("Server is running on port 3333")
+)
